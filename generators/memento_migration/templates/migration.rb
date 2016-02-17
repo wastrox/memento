df77bb6b63c4003aa614eee42c47c59c4984cece
Alexander Kosachov <alexander@kosachov.com>
2016-02-17 21:45:35 +0200

    fixed rspec-mocks' old `:should` deprecation warnings, changed connection adapter to mysql2 because we need string primary keys, updated rails generators to use new db schema

--- generators/memento_migration/templates/migration.rb
+++ generators/memento_migration/templates/migration.rb
@@ -1,18 +1,43 @@
 class MementoMigration < ActiveRecord::Migration
 
-  def change
-    create_table :memento_sessions do |t|
-      t.references :user
+  def self.up
+    create_table :memento_sessions, { :id => false } do |t|
+      t.string :id, limit: 32, index: true
+      t.string :user_id, limit: 32, index: true
       t.timestamps null: false
+      t.string :created_at_ms, limit: 32, index: true
     end
+    execute "ALTER TABLE memento_sessions ADD PRIMARY KEY (id);"
 
-    create_table :memento_states do |t|
+    create_table :memento_states, { :id => false } do |t|
+      t.string :id, limit: 32, index: true
       t.string :action_type
       t.binary :record_data, :limit => 16777215
       t.references :record, :polymorphic => true
       t.references :session
       t.timestamps null: false
+      t.string :created_at_ms, limit: 32, index: true
     end
+    execute "ALTER TABLE memento_states ADD PRIMARY KEY (id);"
+
+    change_column :memento_states, :record_id, :string, limit: 32, index: true
+    change_column :memento_states, :session_id, :string, limit: 32, index: true
+
+
+    create_table :memento_actions, { :id => false } do |t|
+      t.string :id, limit: 32, index: true
+      t.string :session_id, limit: 32, index: true
+      t.string :action
+      t.binary :action_params, :limit => 16777215
+    end
+    execute "ALTER TABLE memento_actions ADD PRIMARY KEY (id);"
+
+  end
+
+  def self.down
+    drop_table :memento_sessions
+    drop_table :memento_states
+    drop_table :memento_actions
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,13 +14,18 @@ $:.unshift(File.dirname(__FILE__))
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'memento'
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+#ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+ActiveRecord::Base.establish_connection(:adapter => "mysql2", :database => "memento_test")
+
 # catch AR schema statements
 $stdout = StringIO.new
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :should
   end
 end
 
@@ -50,18 +55,35 @@ def setup_db
       t.column :name, :string
     end
 
-    create_table :memento_sessions do |t|
-      t.references :user
+    create_table :memento_sessions, { :id => false } do |t|
+      t.string :id, limit: 32, index: true
+      t.string :user_id, limit: 32, index: true
       t.timestamps null: false
+      t.string :created_at_ms, limit: 32, index: true
     end
+    connection.execute "ALTER TABLE memento_sessions ADD PRIMARY KEY (id);"
 
-    create_table :memento_states do |t|
+    create_table :memento_states, { :id => false } do |t|
+      t.string :id, limit: 32, index: true
       t.string :action_type
       t.binary :record_data, :limit => 16777215
       t.references :record, :polymorphic => true
       t.references :session
       t.timestamps null: false
+      t.string :created_at_ms, limit: 32, index: true
     end
+    execute "ALTER TABLE memento_states ADD PRIMARY KEY (id);"
+
+    change_column :memento_states, :record_id, :string, limit: 32, index: true
+    change_column :memento_states, :session_id, :string, limit: 32, index: true
+
+    create_table :memento_actions, { :id => false } do |t|
+      t.string :id, limit: 32, index: true
+      t.string :session_id, limit: 32, index: true
+      t.string :action
+      t.binary :action_params, :limit => 16777215
+    end
+    execute "ALTER TABLE memento_actions ADD PRIMARY KEY (id);"
 
   end
 end
